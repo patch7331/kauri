b1b3e6e90572246915c2ac49376241342ae710d7
Prajna Sariputra <putr4.s@gmail.com>
2019-04-26 16:26:36 +1000

    Load button first attempt in client
    Opening the file dialog, sending and receiving seems to work, however
    actually updating the app contents doesn't, no idea why.

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -6,6 +6,8 @@ import { h, Component } from "preact";
 import Editor from "components/Editor";
 import Header from "components/Header";
 
+const POST_URI_OPEN_FILE = "http://127.0.0.1:3000/load";
+
 export default class App extends Component {
   /**
    * Constructs a new app component.
@@ -14,11 +16,25 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { document: demo.document };
+    this.loadFile = this.loadFile.bind(this);
+  }
+
+  loadFile() {
+    const { dialog } = require("electron").remote;
+    var filePath = dialog.showOpenDialog({
+      properties: ["openFile"],
+    });
+    fetch(POST_URI_OPEN_FILE, { method: "POST", body: filePath[0] })
+      .then(resp => {
+        this.setState(resp.json());
+      })
+      .catch(e => console.log(e));
   }
 
   render() {
     return (
       <div class="app">
+        <button onClick={this.loadFile}>Load</button>
         <Header title={this.state.document.title} />
         <Editor dom={this.state.document} />
       </div>
