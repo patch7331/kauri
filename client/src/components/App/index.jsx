053666fed57c3e4f41540e0a3fcbc636491f75af
Prajna Sariputra <putr4.s@gmail.com>
2019-04-29 18:12:22 +1000

    Moved the opening ODT functionality into a menu item
    Also needed to actually recreate the entire menu ourselves so we still
    have the default options (minus Help)

--- client/src/components/App/index.jsx
+++ client/src/components/App/index.jsx
@@ -8,6 +8,99 @@ import Header from "components/Header";
 import { remote } from "electron";
 
 const POST_URI_OPEN_FILE = "http://127.0.0.1:3000/load";
+// The menu template object here is based on the sample in the electron docs, with the Help menu removed
+function getMenuTemplate(mainApp) {
+  return [
+    // Application menu for macOS
+    ...(process.platform === "darwin"
+      ? [
+          {
+            label: remote.app.getName(),
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideothers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
+    // File menu
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "Open ODT",
+          data: mainApp,
+          click(menuItem, browserWindow, event) {
+            menuItem.data.loadFile();
+          },
+        },
+        process.platform === "darwin" ? { role: "close" } : { role: "quit" },
+      ],
+    },
+    // Edit menu
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(process.platform === "darwin"
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }],
+              },
+            ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+      ],
+    },
+    // View menu
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forcereload" },
+        { role: "toggledevtools" },
+        { type: "separator" },
+        { role: "resetzoom" },
+        { role: "zoomin" },
+        { role: "zoomout" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+    // Window menu
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(process.platform === "darwin"
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" },
+            ]
+          : [{ role: "close" }]),
+      ],
+    },
+  ];
+}
 
 export default class App extends Component {
   /**
@@ -19,6 +112,8 @@ export default class App extends Component {
     this.state = { document: demo.document };
     this.loadFile = this.loadFile.bind(this);
     this.editor = createRef();
+    const menu = remote.Menu.buildFromTemplate(getMenuTemplate(this));
+    remote.Menu.setApplicationMenu(menu);
   }
 
   /**
@@ -40,7 +135,6 @@ export default class App extends Component {
   render(props, state) {
     return (
       <div class="app">
-        <button onClick={this.loadFile}>Load</button>
         <Header title={state.document.title} />
         <Editor ref={this.editor} dom={state.document} />
       </div>
