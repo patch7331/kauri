2f2796e69cc5154295752bbd157086dcde91cc8a
Prajna Sariputra <putr4.s@gmail.com>
2019-05-18 13:08:28 +1000

    Move request matching out of main() to handle_request()

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("Listening on http://{}", addr);
 
     loop {
-        let mut request = match server.recv() {
+        let request = match server.recv() {
             //server.recv() blocks until a request actually comes
             Ok(rq) => rq,
             Err(e) => {
@@ -18,42 +18,46 @@ fn main() {
                 break;
             }
         };
-        match request.url() {
-            //check the URL and respond accordingly
-            "/load" => {
-                let req_reader = request.as_reader();
-                let mut body_bytes: Vec<u8> = Vec::new();
-                if let Err(e) = req_reader.read_to_end(&mut body_bytes) {
-                    println!("error: {}", e);
-                    continue;
-                }
-                let body_str = std::str::from_utf8(&body_bytes);
-                if let Err(e) = body_str {
-                    println!("error: {}", e);
-                    continue;
-                }
-                let parser = ODTParser::new(body_str.unwrap());
-                if let Err(e) = parser {
-                    println!("error: {}", e);
-                    continue;
-                }
-                let parsed_odt = parser.unwrap().parse();
-                if let Err(e) = parsed_odt {
-                    println!("error: {}", e);
-                    continue;
-                }
-                let response = tiny_http::Response::from_string(parsed_odt.unwrap());
-                if let Err(e) = request.respond(response) {
-                    println!("error: {}", e);
-                    continue;
-                }
+        handle_request(request);
+    }
+}
+
+/// Takes a request and responds accordingly
+fn handle_request(mut request: tiny_http::Request) {
+    match request.url() {
+        "/load" => {
+            let req_reader = request.as_reader();
+            let mut body_bytes: Vec<u8> = Vec::new();
+            if let Err(e) = req_reader.read_to_end(&mut body_bytes) {
+                println!("error: {}", e);
+                return;
             }
-            _ => {
-                let response = tiny_http::Response::empty(404);
-                if let Err(e) = request.respond(response) {
-                    println!("error: {}", e);
-                    continue;
-                }
+            let body_str = std::str::from_utf8(&body_bytes);
+            if let Err(e) = body_str {
+                println!("error: {}", e);
+                return;
+            }
+            let parser = ODTParser::new(body_str.unwrap());
+            if let Err(e) = parser {
+                println!("error: {}", e);
+                return;
+            }
+            let parsed_odt = parser.unwrap().parse();
+            if let Err(e) = parsed_odt {
+                println!("error: {}", e);
+                return;
+            }
+            let response = tiny_http::Response::from_string(parsed_odt.unwrap());
+            if let Err(e) = request.respond(response) {
+                println!("error: {}", e);
+                return;
+            }
+        }
+        _ => {
+            let response = tiny_http::Response::empty(404);
+            if let Err(e) = request.respond(response) {
+                println!("error: {}", e);
+                return;
             }
         }
     }
