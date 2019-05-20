ff3c7313285c81aa27f14dcc8365135daf39d2f5
David <koflropter96@gmail.com>
2019-05-20 20:36:51 +1000

    made suggested changes

--- client/src/renderer/components/Clipboard/index.jsx
+++ client/src/renderer/components/Clipboard/index.jsx
@@ -12,59 +12,60 @@ import clipboard from "electron-clipboard-extended";
 export default class Clipboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { clipboardStack: [] };
+    this.state = {clipboardStack: []};
     this.handleTextChanged = this.handleTextChanged.bind(this);
     this.handleImageChanged = this.handleImageChanged.bind(this);
   }
 
   componentDidMount() {
     clipboard.startWatching();
-    clipboard.on("text-changed", handleTextChanged());
-    clipboard.on("image-changed", handleImageChanged());
+    clipboard.on("text-changed", this.handleTextChanged);
+    clipboard.on("image-changed", this.handleImageChanged);
+  }
 
   componentWillUnmount() {
     clipboard.stopWatching();
     clipboard.off("text-changed");
   }
 
-    handleTextChanged() {
-      this.setState(prevState => {
-        return {
-          clipboardStack: [
-            ...prevState.clipboardStack,
-            new ClipboardItem("txt", clipboard.readText()),
-          ],
-        };
-      });
-    }
+  handleTextChanged() {
+    this.setState(prevState => {
+      return {
+        clipboardStack: [
+          ...prevState.clipboardStack,
+          new ClipboardItem("txt", clipboard.readText()),
+        ],
+      };
+    });
+  }
 
-    handleImageChanged() {
-      this.setState(prevState => {
-        const img = clipboard.readImage();
-        const imgURI = img.toDataURL();
-        return {
-          clipboardStack: [
-            ...prevState.clipboardStack,
-            new ClipboardItem("img", imgURI),
-          ],
-        };
-      });
-    };
+  handleImageChanged() {
+    this.setState(prevState => {
+      const img = clipboard.readImage();
+      const imgURI = img.toDataURL();
+      return {
+        clipboardStack: [
+          ...prevState.clipboardStack,
+          new ClipboardItem("img", imgURI),
+        ],
+      };
+    });
+  };
 
 
   render(props, state) {
     return (
-      <ul class="clipboard-list">
-        {state.clipboardStack.map(item => (
-          <li class="clipboard-item">
-            {item.type === "txt" ? (
-              <p>{item.data}</p>
-            ) : (
-              <img src={item.data} alt="image" />
-            )}
-          </li>
-        ))}
-      </ul>
+        <ul class="clipboard-list">
+          {state.clipboardStack.map(item => (
+              <li class="clipboard-item">
+                {item.type === "txt" ? (
+                    <p>{item.data}</p>
+                ) : (
+                    <img src={item.data} alt="image"/>
+                )}
+              </li>
+          ))}
+        </ul>
     );
   }
 }
