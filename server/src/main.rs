d99abc2fa51fc4b4fe293b50a96bde8425a6b1c9
Sean Bailey <sean@seanbailey.io>
2019-07-24 10:59:12 +1000

    Add module docs to main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,24 @@
+//! Kauri daemon
+//!
+//! The following module contains the Kauri daemon. A simple HTTP server that
+//! runs silently in the background, providing performance critical services to
+//! the Electron front end. This portion of the project most notable includes
+//! the following modules:
+//!
+//! - A basic `document` implementation.
+//! - Parsers for common document formats (`parsers`).
+//! - Exporters for common file formats.
+
+#![warn(missing_docs)]
+
 extern crate tiny_http;
 
 mod controllers;
 mod document;
 mod parsers;
 
+/// Main entry point
+/// Establishes the simple HTTP server, and listens for requests.
 fn main() {
     let addr = "127.0.0.1:3000";
     let server = tiny_http::Server::http(addr).unwrap();
