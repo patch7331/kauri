eb79cadfedb90520619c34951df1b00c0723a02c
luke5599 <39188268+luke5599@users.noreply.github.com>
2019-08-14 16:21:37 +1000

    Working jest test on commands Reducer

--- client/test/commandReducer_test.js
+++ client/test/commandReducer_test.js
@@ -1,26 +1,33 @@
-import commandReducer from '../src/renderer/redux/reducers/command';
-import { ADD_COMMAND } from '../src/renderer/redux/actionTypes';
+/** @format */
 
-describe('command reducer', () => {
-    it('should handle ADD_COMMAND', () => {
-        expect( 
-            commandReducer([], {
-                type: ADD_COMMAND,
-                payload: {
-                    id: "Clipboard:copy",
-                    name: "copy",
-                    keys: "CmdOrCtrl+C",
-                    behaviour: "this.doClipboardCopy",
-                },
-            })
-        ).toEqual([
-            {
-                id: "Clipboard:copy",
-                name: "copy",
-                keys: "CmdOrCtrl+C",
-                behaviour: "this.doClipboardCopy",
-            }
-        ])
-    })
-})
+import commandReducer from "../src/renderer/redux/reducers/commands";
+import * as type from "../src/renderer/redux/actionTypes";
 
+describe("command reducer", () => {
+  it("should handle ADD_COMMAND", () => {
+    expect(
+      commandReducer(
+        { allIds: [], byId: {} },
+        {
+          type: type.ADD_COMMAND,
+          payload: {
+            id: "Clipboard:copy",
+            name: "copy",
+            keys: "CmdOrCtrl+C",
+            behaviour: "this.doClipboardCopy",
+          },
+        }
+      )
+    ).toEqual({
+      allIds: ["Clipboard:copy"],
+      byId: {
+        "Clipboard:copy": {
+          id: "Clipboard:copy",
+          name: "copy",
+          keys: "CmdOrCtrl+C",
+          behaviour: "this.doClipboardCopy",
+        },
+      },
+    });
+  });
+});
