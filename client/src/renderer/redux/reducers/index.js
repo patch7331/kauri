641fd6a3afd84ea9dea3c761a956d8236b5e733d
luke5599 <39188268+luke5599@users.noreply.github.com>
2019-08-21 14:17:53 +1000

    Add metadata reducer with constant json (#97)

--- client/src/renderer/redux/reducers/index.js
+++ client/src/renderer/redux/reducers/index.js
@@ -4,5 +4,6 @@ import { combineReducers } from "redux";
 import caretPosition from "./caretPosition";
 import commands from "./commands";
 import document from "./document";
+import metadata from "./metadata";
 
-export default combineReducers({ caretPosition, commands, document });
+export default combineReducers({ caretPosition, commands, document, metadata });
---
+++ client/src/renderer/redux/reducers/metadata.js
@@ -0,0 +1,5 @@
+/** @format */
+
+import test from "./metadata.json";
+
+export default (state = test, action) => state;
