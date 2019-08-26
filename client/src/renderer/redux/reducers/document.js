e2a9bc732a82bda8c1ee9e9334ba88dc8266c7cb
unknown <patch7331@gmail.com>
2019-08-26 19:02:03 +1000

    Pull request changes

--- client/src/renderer/redux/reducers/document.js
+++ client/src/renderer/redux/reducers/document.js
@@ -10,34 +10,50 @@ const intialState = {
   nodes: translate(test),
 };
 
+/**
+ * Recursively traveses json tree, storing them in an array after
+ * they have been assinged an ID
+ * @param {array} nodes
+ */
 function translate(nodes) {
   let id = 0;
-  const nextId = () => id++;
   const byId = {};
+
+  // Recursive callbacks
+  const nextId = () => id++;
   const addToById = node => (byId[node.id] = node);
   const allIds = nodes.map(node => translateNode(node, nextId, addToById));
   return { byId, allIds };
 }
 
+/**
+ * Flattens nodes and assigns them IDs, recursively travels to
+ * child nodes
+ * @param {array} node 
+ * @param {number} nextId 
+ * @param {array} addToById 
+ */
 function translateNode(node, nextId, addToById) {
+  //Handles text shorthand
   if (typeof node === "string") {
     node = {
       type: "text",
       content: node,
     };
   }
+
   node.id = nextId();
   if (node.children) {
     node.children = node.children.map(node =>
       translateNode(node, nextId, addToById),
     );
   }
+
   addToById(node);
   return node.id;
 }
 
 export function caretReducer(state = initialState, action) {
-  console.log(action);
   switch (action.type) {
     case UPDATE_CARET_POSITION:
       return {
